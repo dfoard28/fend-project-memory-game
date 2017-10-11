d69239ef24099756b7bb92408a5c16610d4e64c0
Desiree Foard <foard.desiree@gmail.com>
2017-10-11 17:57:24 -0600

    started the display cards portion of the process with a click function on the start button that shuffles the card var. Also set up rudimentry functions for what happens when a card is clicked.

--- js/app.js
+++ js/app.js
@@ -2,30 +2,34 @@
  * Create a list that holds all of your cards
  */
 var cards = [{
-    id: 1,
-    match: 3},
-    {id: 2,
-    match: 4},
-    {id: 3,
-     match: 1},
-    {id: 4,
-      match: 2
+        id: 1,
+        icon: 'fa fa-paper-plane-o',
+        match: 3},
+    {
+        id: 2,
+        icon: 'fa fa-diamond',
+        match: 4
+    },
+    {
+        id: 3,
+        icon:'fa fa-paper-plane-o',
+        match: 1
+    },
+    {   
+        id: 4,
+        icon: 'fa fa-diamond',
+        match: 2
     }]
 
+//Display cards
 $('.start').click(function(){
+ //on click of start button call the shuffle function to shuffle the array
     shuffle(cards);
     console.log(cards);
-    cards.forEach(function(){
-        $('li').addClass('.show');  
-    })
+//loop through each card and created its HTML
+//add each cards HTML to the page
 })
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -43,13 +47,32 @@ function shuffle(array) {
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+//setup event listener for when card is clicked
+$('li').click(function(){
+//call showSymbol function
+//call openCardList function
+
+})
+//display the cards symbol (maybe to this as a toggleClass?)
+function showSymbol(){
+
+}
+//add card to a list of open cards
+//if list already has another card check to see if the two cards match
+function openCardList(){
+//if cards match then call lockCards function and add the match class
+//if cards do not match then remove them from the list and call showSymbol function to hide symbol
+}
+//lock cards in an open postion if they match from the openCardsList
+function lockCards(){
+
+}
+//increment move counter and display it on the page
+var moveCounter = 0;
+function counterDisplay(){
+
+}
+//if all cards match display a message with the final score
+function allMatched(){
+
+}
